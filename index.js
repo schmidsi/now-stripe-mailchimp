2094111f983a78e0042c12a008a91e823d22fda1
Simon Schmid <schmidsi@simonschmids-macbook.local>
2017-03-09 17:38:35 +0100

    clean up

--- index.js
+++ index.js
@@ -3,11 +3,26 @@ require('dotenv').config()
 const { send } = require('micro')
 const parse = require('urlencoded-body-parser')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const rp = require('request-promise')
 
 
 module.exports = async function (req, res) {
   const data = await parse(req)
-  console.log(data)
+
+  const mailchimpAnswer = await rp({
+    method: 'POST',
+    auth: {
+      'user': 'stripe-microservice',
+      'pass': process.env.MAILCHIMP_API_KEY,
+    },
+    uri: 'https://us15.api.mailchimp.com/3.0/lists/f00ee7fa32/members/',
+    body: {
+        "email_address": data.stripeEmail,
+        "status": "subscribed",
+    },
+    json: true,
+    simple: false,
+  })
 
   const charge = stripe.charges.create({
     amount: 2000,
@@ -15,13 +30,16 @@ module.exports = async function (req, res) {
     source: data.stripeToken,
     description: 'skinsure deposit - opus néoi GmbH',
   }, function(err, charge) {
-    // console.log(err, charge);
+    console.log('new deposit!', {
+      email: data.stripeEmail,
+      mailchimp: mailchimpAnswer.title || mailchimpAnswer.status,
+      stripeId: charge.id
+    })
 
     res.writeHead(302, {
-    'Location': 'http://opus-neoi.webflow.io/success'
-  });
-  res.end();
+      'Location': 'http://opus-neoi.webflow.io/success'
+    });
 
-    // send(res, 302, 'done');
+    res.end();
   })
 }
